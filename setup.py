d1cd7e31fe370c77fd83aa9bef8e78792d21a5ac
Adam Johnson <me@adamj.eu>
2018-02-16 11:05:41 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     install_requires=[],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license="ISC license",
     zip_safe=False,
     entry_points={
